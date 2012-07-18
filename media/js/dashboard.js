816e6df36a1968a12ac89c5842aaa91a3b875255
James Davis <jdavis@dyn-butler-159-144.dyn.columbia.edu>
2012-07-18 14:52:36 -0400

    adding more dashboard locking

--- media/js/dashboard.js
+++ media/js/dashboard.js
@@ -4,7 +4,6 @@
 
 var links = jQuery('.section_url');
 var uninitiated = []
-
 links.each(function(){
 	var link = jQuery(this);
 	var class_name = link.parent().next().children().attr('class')
@@ -14,23 +13,37 @@ links.each(function(){
 })
 
 jQuery(uninitiated).each(function(i){
-	if(i === 0){
-	}
-	else if(jQuery(this).hasClass('module') ){
-
+	if(jQuery(this).hasClass('module') ){
+		//do not replace link
 	}
 	else if(jQuery(this).hasClass('part') ){
-
+		//do not replace link
+	}
+	else if(typeof next_section_status == "boolean"){
+		if(next_section_status == true){
+			//do not replace link
+			next_section_status = false;
+		}
+		else{
+			replaceLink(jQuery(this) );	
+		}
 	}
 	else{
-		var link = jQuery(this);
+		replaceLink(jQuery(this) );	
+	}
+	function replaceLink(link){
+		//replace link
 		var linkSpan = '<span>'+ link.html() +'</span>'
 		link.parent().append(linkSpan)
 		link.remove()
 	}
 }) 
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function(){ 
+
+/*
+* Initiate the bootstrap popup
+*/
 	$(".module-info-popover").popover({
     	offset: 10,
     	trigger:'manual'
