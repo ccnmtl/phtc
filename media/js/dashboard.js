2549c5fbbc3508eedf384ced1766e47767aa40ab
Anders Pearson <anders@columbia.edu>
2012-08-10 13:14:59 -0400

    4-spaces for js and semicolons required

--- media/js/dashboard.js
+++ media/js/dashboard.js
@@ -1,52 +1,52 @@
 jQuery(document).ready(function(){ 
-/*
-** Initiate the bootstrap popup
-*/
-function add_bootstrap_popup(){
-	$(".module-info-popover").popover({
-    	offset: 10,
-    	trigger:'manual'
+    /*
+    ** Initiate the bootstrap popup
+    */
+    function add_bootstrap_popup(){
+        $(".module-info-popover").popover({
+            offset: 10,
+            trigger:'manual'
 
-    }).click(function(){
-         $(this).popover('show')
+        }).click(function(){
+            $(this).popover('show');
 
-    }).mouseleave(function(){
-    	$(this).popover('hide')
-    });
+        }).mouseleave(function(){
+            $(this).popover('hide');
+        });
 
-	$(".section-info-popover").popover({
-    	offset: 10,
-    	trigger:'manual'
+        $(".section-info-popover").popover({
+            offset: 10,
+            trigger:'manual'
 
-    }).click(function(){
-         $(this).popover('show')
+        }).click(function(){
+            $(this).popover('show');
 
-    }).mouseleave(function(){
-    	$(this).popover('hide')
-    });
-}
+        }).mouseleave(function(){
+            $(this).popover('hide');
+        });
+    }
 
- /*
- ** Module/Part UI 
- */
- $('.show-module').click(function(){
- 	var link = $(this).attr('rel')
- 	var id = $(this).parent().attr('id')
-    jQuery('.content').append('<div id="page-load"><h1>Loading...</h1</div>')
-  	$.ajax({
-		url: link,
-		mod_id: id,
-		type: "POST",
-		data: { module: link, mod_id: id }
-		}).done(function(data) {
+    /*
+    ** Module/Part UI 
+    */
+    $('.show-module').click(function(){
+        var link = $(this).attr('rel');
+        var id = $(this).parent().attr('id');
+        jQuery('.content').append('<div id="page-load"><h1>Loading...</h1</div>');
+        $.ajax({
+            url: link,
+            mod_id: id,
+            type: "POST",
+            data: { module: link, mod_id: id }
+        }).done(function(data) {
             
-			$('#return').empty().load('/dashboard_panel/ #part_id_'+ id, function(response, status, xhr){
-				if(status == "success"){
-					$('#return').children().children().css('display','block')
+            $('#return').empty().load('/dashboard_panel/ #part_id_'+ id, function(response, status, xhr){
+                if(status == "success"){
+                    $('#return').children().children().css('display','block');
                     add_bootstrap_popup();
-				}
-            jQuery('#page-load').remove();
-			})
-		});
- 	})//end click
+                }
+                jQuery('#page-load').remove();
+            });
+        });
+    });//end click
 });//end doc.ready
