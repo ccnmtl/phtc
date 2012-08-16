feaeba2ef80e97bb20d9e701741e2d8d61d85fea
Anders Pearson <anders@columbia.edu>
2012-08-16 14:07:25 -0400

    flake8 compliance

--- phtc/main/views.py
+++ phtc/main/views.py
@@ -9,7 +9,6 @@ from phtc.main.models import UserProfile
 from phtc.main.forms import UserRegistrationForm
 from phtc.main.models import DashboardInfo
 from pagetree.models import UserPageVisit
-from django.core.mail import EmailMessage
 from django.core.mail import EmailMultiAlternatives
 
 
@@ -63,26 +62,41 @@ def user_visits(request):
 
 
 def send_post_test_email(user, section, module, request):
-    subject, from_email, to = 'Public Health Training Diploma', 'no-reply@lowernysphtc.org', user.email
+    (subject, from_email, to) = (
+        'Public Health Training Diploma',
+        'no-reply@lowernysphtc.org',
+        user.email)
     text_content = ''
-    html_content = ('<p>Congratulations on successfully completing the online training program ' +module.label+ '.</p>'
-                    '<p>You may now access your certificate of completion for this program. Simply, '+
-                    '<a href="http://' + request.get_host() + '/dashboard/">click here</a> '+
-                    'to return to your personal dashboard; a link to your certificate is '+
-                    '<a href="'+ 'http://' + request.get_host() + '/certificate' + module.get_absolute_url()+
-                    '">here</a>.</p>'+
-                    '<p>To request continuing education credit for this training program, please write to '+
-                    'phtc@columbia.edu. In an email, please include your name, contact information, and the type '+
-                    'of credit you are requesting, and a staff member of the New York City-Long Island-Lower Tri '+
-                    'County Public Health Training Center will follow-up with you shortly.</p>'+
-                    '<p>If you experience any technical difficulties in accessing the certificate or the dashboard, '+
-                    'please also contact phtc@clumbia.edu.</p>'+
-                    '<p>Thank you for choosing the New York City-Long Island-Lower Tri County Public Health Training '+
-                    'Center. We hope you will return to our site often and take advantage of new content and other '+
-                    'training offerings</p>'+
-                    '<p>New York City-Long Island-Lower Tri-County Public Health Training Center</br>'+
-                    'Columbia University | Mailman School of Public Health</br>722 West 168th Street, Room 552<br/>'+
-                    'New York, NY 10032</br>Phone: (212) 305-6984</br>Fax: (212) 342-9004</br>Email: phtc@columbia.edu</p>')
+    # this really should go in a template instead of being inlined
+    html_content = (
+        '<p>Congratulations on successfully completing the online '
+        'training program ' + module.label + '.</p>'
+        '<p>You may now access your certificate of completion for '
+        'this program. Simply, ' + '<a href="http://' +
+        request.get_host() + '/dashboard/">click here</a> ' +
+        'to return to your personal dashboard; a link to your '
+        'certificate is ' + '<a href="' + 'http://' +
+        request.get_host() + '/certificate' + module.get_absolute_url() +
+        '">here</a>.</p>' + '<p>To request continuing education credit '
+        'for this training program, please write to ' +
+        'phtc@columbia.edu. In an email, please include your name, '
+        'contact information, and the type ' +
+        'of credit you are requesting, and a staff member of the New '
+        'York City-Long Island-Lower Tri ' +
+        'County Public Health Training Center will follow-up with you '
+        'shortly.</p><p>If you experience any technical difficulties in'
+        ' accessing the certificate or the dashboard, ' +
+        'please also contact phtc@clumbia.edu.</p>' +
+        '<p>Thank you for choosing the New York City-Long Island-Lower '
+        'Tri County Public Health Training ' +
+        'Center. We hope you will return to our site often and take '
+        'advantage of new content and other ' +
+        'training offerings</p>' +
+        '<p>New York City-Long Island-Lower Tri-County Public Health '
+        'Training Center</br>Columbia University | Mailman School of '
+        'Public Health</br>722 West 168th Street, Room 552<br/>' +
+        'New York, NY 10032</br>Phone: (212) 305-6984</br>Fax: (212) '
+        '342-9004</br>Email: phtc@columbia.edu</p>')
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
@@ -382,8 +396,9 @@ def certificate(request, path):
     module = get_module(section)
     is_visited = user_visits(request)
     #return HttpResponse(module.get_uservisit(request.user).status )
-    #make sure this page is only viewable if the module is completed. 
-    if module.get_uservisit(request.user) and module.get_uservisit(request.user).status == "complete":
+    #make sure this page is only viewable if the module is completed.
+    if (module.get_uservisit(request.user)
+        and module.get_uservisit(request.user).status == "complete"):
         return dict(section=section,
                 module=module,
                 is_visited=is_visited,
