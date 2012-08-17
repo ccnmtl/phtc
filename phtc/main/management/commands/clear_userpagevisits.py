ef479f80c5432bc0ea85ccb76e26e56f21a97b15
James Davis <jdavis@dyn-butler-159-65.dyn.columbia.edu>
2012-08-17 14:04:41 -0400

    added an option to clear_userpagevisits that accepts username as argument implement --username=<your username>

--- phtc/main/management/commands/clear_userpagevisits.py
+++ phtc/main/management/commands/clear_userpagevisits.py
@@ -1,11 +1,33 @@
 from django.core.management.base import BaseCommand
+from optparse import make_option
 from django.conf import settings
+from django.contrib.auth.models import User
 from pagetree.models import UserPageVisit
+from phtc.main.models import UserProfile
 
 class Command(BaseCommand):
-    args = '<userpagevisit>'
+    args = '<username>'
     help = 'clear out all user page visits'
 
+    option_list = BaseCommand.option_list + (
+        make_option('--username', 
+            action='store',
+            type='string',
+            default=False,
+            help='Delete specific user history'),
+        )
+
     def handle(self, *args, **options):
-    	UserPageVisit.objects.all().delete()
-    	print "done clearing out the user page visits"
+    	if options['username']:
+    		username = options['username']
+    		try:
+    			user = User.objects.get(username__exact=username)
+    			upv = UserPageVisit.objects.filter(user_id = user.id)
+    			for v in upv:
+    				v.delete()
+    		except:
+    			pass
+    		print "done clearing out " + username +"\'s user page visits"
+    	else:
+    		UserPageVisit.objects.all().delete()
+    		print "done clearing out all user page visits"
