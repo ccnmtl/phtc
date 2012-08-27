0241e0e319fa88654ec827b342e95da5340e2877
Zarina Mustapha <zarina@columbia.edu>
2012-08-27 12:34:56 -0400

    just obsessive commit of design-notation js

--- media/js/design-notation.js
+++ media/js/design-notation.js
@@ -1,10 +1,21 @@
 jQuery(document).ready(function(){
 
-    function create_designform() {
-        var this_scenario = jQuery('.interactive-scenario').attr('id');
-        jQuery('.interactive-scenario').append('<form/>');
-        jQuery('.interactive-scenario form').attr('id', this_scenario);   
+    var this_scenario = jQuery('.interactive-scenario').attr('id');
+    var this_scenario = this_scenario + 'form';
+    
+    function generaterow() {
+        for (i=0; i<4; i++) {
+            jQuery('form').append('<input type="text" size="1" maxlength="1" />');
+        }
     }
+
+    function create_designform() {
+        jQuery('.interactive-scenario').append('<form/>');
+        jQuery('.interactive-scenario form').attr('id', this_scenario);
+        generaterow();
+    }
+    
+    
     
     create_designform();
     
