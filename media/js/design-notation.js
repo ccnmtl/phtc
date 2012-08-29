933f6b9c24d1debbfd26d1693fa0143b8861139f
Zarina Mustapha <zarina@columbia.edu>
2012-08-29 12:37:20 -0400

    elaborate design-notation exercise

--- media/js/design-notation.js
+++ media/js/design-notation.js
@@ -1,82 +1,141 @@
-var scenario = jQuery('.interactive-scenario').attr('id');
-var this_scenario = scenario + 'form';
+jQuery(document).ready(function() {
 
-function generaterow() {
-    for (i=0; i<2; i++) {
-        for (j=0; j<4; j++) {
-            if (j==0) {
-                jQuery('form').append('<input name="notation" type="text" size="1" maxlength="2" id="r'+i+'c'+j+'" />');
-            }
-            else {
-                jQuery('form').append('<input name="notation" type="text" size="1" maxlength="1" id="r'+i+'c'+j+'" />');
-            }
-        }
-    jQuery('form').append('<br />');
+var scenario;
+var colNum = 0;
+var rowNum = 0;
+var cellTotal = 0;
+var scenario2_answer = ' '+'O'+'X'+'O'+' '+' '+' '+' ';
+var scenario3_answer = 'R'+'O'+'X'+'O'+'R'+'O'+' '+'O';
+var finaldesign_answer = 'NR' + 'O' + 'X' + 'O' + 'O' + 'NR' + 'O' + ' ' + 'O' + 'O';
+var correctAnswer = "";
+
+function initScenario() {
+   scenario = jQuery('.interactive-scenario').attr('id');
+   if (scenario=='scenario2') {
+       colNum = 4;
+       rowNum = 2;
+       cellTotal = 8;
+       correctAnswer = scenario2_answer;
+   }
+   if (scenario=='scenario3') {
+       colNum = 4;
+       rowNum = 2;
+       cellTotal = 8;
+       correctAnswer = scenario3_answer;
+   }
+   if (scenario=='finaldesign') {
+       colNum = 5;
+       rowNum = 2;
+       cellTotal = 10;
+       correctAnswer = scenario3_answer;
+   }
+}
+
+function generateSelector() {
+    var notationValue = ['NR','R','X','O','&nbsp;'];
+    var divCell = [];
+    jQuery('.interactive-scenario').append('<div class="notation_select" id="notation_selections">');
+    for (i=0; i<notationValue.length; i++) {
+        divCell[i] = '<div class="notation_option btn btn-primary">'+notationValue[i]+'</div>';
+        jQuery('.notation_select').append(divCell[i]);
     }
 }
 
-function create_designform() {
-    jQuery('.interactive-scenario').append('<form/>');
-    jQuery('.interactive-scenario form').attr('id', this_scenario);
-    generaterow();
+function generateGrid() {
+    for (i=0; i<2; i++) {
+        jQuery('.interactive-scenario').append('<div class="notation_row">');
+    }
+    for (j=0; j<colNum; j++) {
+        jQuery('.notation_row').append('<div class="notation_field" id="r'+i+'c'+j+'" />')
+    }
     jQuery('.interactive-scenario').append('<button id="checkanswer" type="submit" class="btn">Check answer</button>');
 }
 
-function check_input_validity() {
-    jQuery('input').keyup(function() {
-    var value = jQuery(this).val();
-    if (value!="") {
-        if (value=="0") { value = jQuery(this).val('o');}
-        if (value=="-") { value = jQuery(this).val(' ');}
-        if (value=="n" || value == "N") { value = jQuery(this).val('NR');}
-        value = jQuery(this).val();
-        value = value.toUpperCase();
-        if (value=='O' || value=='X' || value=='R' || value=='NR' || value==' ') {
-         jQuery(this).addClass('hasvalue');
-       }
-       else {
-           alert('You have entered an illegal value "' + value +'". Please read the instructions and try again.');
-       }
-    }
-    else if (jQuery(this).attr('class')) {jQuery(this).removeClass();}
+function show_notation_select() {
+    var srcElement;
+    jQuery(document).click(function() {
+        jQuery('.notation_select').removeClass('visible');
+        jQuery('.notation_field').removeClass('active');
     });
- }
-
-function check_answer() {
-    var scenario2_answer = ' '+'O'+'X'+'O'+' '+' '+' '+' ';
-    var scenario3_answer = 'R'+'O'+'X'+'O'+'R'+'O'+' '+'O';
-    jQuery('#checkanswer').click(function() {
-        // get the values of the input
-        var useranswer = "";
-        jQuery('.hasvalue').each(function(){
-            useranswer+=(jQuery(this).val());            
-        });
-        useranswer = useranswer.toUpperCase();
-        
-        if (useranswer==scenario2_answer) {alert('correct');}
-        else {alert('my cat is smarter than you are');}
-        
-      //  console.log(useranswer);
-      //  var useranswer = [];
-     //   jQuery('.hasvalue').each(function(){
-     //       useranswer.push(jQuery(this).val());            
-     //   });
-     //   console.log(useranswer);
-     // check for null values, if null (not blank or -), return error saying user needs to completer everything
-     // transform to lower case or uppercase, convert 0s if necessary
-     // compare to the correct answer
-     // return some kind of answer
-     // 
-     // 
+    
+    jQuery('.notation_field').click(function(evt) {
+        jQuery('.notation_field').removeClass('active');
+        jQuery('.notation_select').addClass('visible');
+        evt.stopPropagation();
+        srcElement = evt.srcElement || evt.target || evt.originalTarget;
+        jQuery(srcElement).addClass('active');
+    });
+    
+    jQuery('.notation_option').click(function(evt) {
+        evt.stopPropagation();
+        var notation_value = jQuery(this).html();
+        jQuery(srcElement).html(notation_value);
+        jQuery('.notation_select').removeClass('visible');
+        jQuery('.notation_field').removeClass('active');
+        jQuery(srcElement).addClass('hasvalue');
     });
 }
-jQuery(document).ready(function(){
 
-    create_designform();
-    check_input_validity();
-    check_answer();
+function resetFeedback() {
+    jQuery('.notation_feedback').removeClass('visible');
+    jQuery('.no_entry').removeClass('visible');
+    jQuery('.notation_correct').removeClass('visible');
+    jQuery('.notation_incorrect').removeClass('visible');
+    jQuery('.notation_undetermined').removeClass('visible');
+    jQuery('.notation_answerkey').removeClass('visible');
+}
 
+function check_answer() {
+    jQuery('#checkanswer').click(function() {
+        var useranswer = "";
+        var blank_answer = 0;
+        jQuery('.notation_field').each(function(index){
+            if (jQuery(this).html()=='') {
+                blank_answer++;
+            }
+            if (jQuery(this).html()=='&nbsp;') {
+                jQuery(this).html(' ');
+            }
+            useranswer+=(jQuery(this).html());
+        });
+        
+        if (blank_answer==cellTotal) {
+            resetFeedback();
+            jQuery('.notation_feedback').addClass('visible');
+            jQuery('.no_entry').addClass('visible');
+        }
+        else if ((blank_answer<8)&&(blank_answer!=0)) {
+            resetFeedback();
+            jQuery('.notation_feedback').addClass('visible');
+            jQuery('.notation_undetermined').addClass('visible');
+        }
+        else {
+            if (useranswer == correctAnswer) {
+                resetFeedback();
+                jQuery('.notation_feedback').addClass('visible');
+                jQuery('.notation_correct').addClass('visible');
+            }
+            else {
+                resetFeedback();
+                jQuery('.notation_feedback').addClass('visible');
+                jQuery('.notation_incorrect').addClass('visible');
+            }
+        }
     
-});//end doc.ready
+    });
+}
 
+function showAnswerkey() {
+    jQuery('#show_answerkey_grid').click(function() {
+        jQuery('.notation_answerkey').addClass('visible');
+    });
+}
+
+    initScenario();
+    generateSelector();
+    generateGrid();
+    show_notation_select();
+    check_answer();
+    showAnswerkey();
+});
 
