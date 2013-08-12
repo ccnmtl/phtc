12432b44e7988e022f2189b769d5a0c4c3db37ae
Anders Pearson <anders@columbia.edu>
2013-08-12 14:43:36 -0400

    test set_row_total()

--- phtc/main/tests/test_views.py
+++ phtc/main/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test.client import Client
+from phtc.main.views import set_row_total
 
 
 class SimpleViewTest(TestCase):
@@ -9,3 +10,31 @@ class SimpleViewTest(TestCase):
     def test_index(self):
         result = self.c.get("/")
         self.assertEqual(result.status_code, 302)
+
+
+class FakeCompleter(object):
+    sex = "male"
+
+
+class TestUtilFunctions(TestCase):
+    def test_set_row_total(self):
+        completer = FakeCompleter()
+        items = {
+            0: dict(Male=0, Female=0),
+            6: dict(Male=0, Female=0)
+        }
+        r = set_row_total(completer, items, 0)
+        self.assertEqual(r[0]['Male'], 1)
+        self.assertEqual(r[6]['Male'], 1)
+        self.assertEqual(r[0]['Female'], 0)
+        self.assertEqual(r[6]['Female'], 0)
+        completer.sex = "female"
+        items = {
+            0: dict(Male=0, Female=0),
+            6: dict(Male=0, Female=0)
+        }
+        r = set_row_total(completer, items, 0)
+        self.assertEqual(r[0]['Male'], 0)
+        self.assertEqual(r[6]['Male'], 0)
+        self.assertEqual(r[0]['Female'], 1)
+        self.assertEqual(r[6]['Female'], 1)
