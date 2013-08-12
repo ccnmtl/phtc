bf07b5b9f57b6871378964cb76826bff50d58b7b
Anders Pearson <anders@columbia.edu>
2013-08-12 14:56:22 -0400

    test calculate_age_gender()

--- phtc/main/tests/test_views.py
+++ phtc/main/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test.client import Client
 from phtc.main.views import set_row_total
+from phtc.main.views import calculate_age_gender
 
 
 class SimpleViewTest(TestCase):
@@ -13,7 +14,9 @@ class SimpleViewTest(TestCase):
 
 
 class FakeCompleter(object):
-    sex = "male"
+    def __init__(self, sex="male", age="Under 20"):
+        self.sex = sex
+        self.age = age
 
 
 class TestUtilFunctions(TestCase):
@@ -38,3 +41,51 @@ class TestUtilFunctions(TestCase):
         self.assertEqual(r[6]['Male'], 0)
         self.assertEqual(r[0]['Female'], 1)
         self.assertEqual(r[6]['Female'], 1)
+
+    def test_calculate_age_gender(self):
+        completers = [
+            FakeCompleter(),
+            FakeCompleter(sex="female"),
+            FakeCompleter(age="20-29"),
+            FakeCompleter(age="30-39"),
+            FakeCompleter(age="40-49"),
+            FakeCompleter(age="50-59"),
+            FakeCompleter(age="60-69"),
+        ]
+        items = [
+            dict(Male=0, Female=0, Total=0),
+            dict(Male=0, Female=0, Total=0),
+            dict(Male=0, Female=0, Total=0),
+            dict(Male=0, Female=0, Total=0),
+            dict(Male=0, Female=0, Total=0),
+            dict(Male=0, Female=0, Total=0),
+            dict(Male=0, Female=0, Total=0),
+        ]
+        r = calculate_age_gender(completers, items)
+        self.assertEqual(r[0]['Male'], 1)
+        self.assertEqual(r[0]['Female'], 1)
+        self.assertEqual(r[0]['Total'], 2)
+
+        self.assertEqual(r[1]['Male'], 1)
+        self.assertEqual(r[1]['Female'], 0)
+        self.assertEqual(r[1]['Total'], 1)
+
+        self.assertEqual(r[2]['Male'], 1)
+        self.assertEqual(r[2]['Female'], 0)
+        self.assertEqual(r[2]['Total'], 1)
+
+        self.assertEqual(r[3]['Male'], 1)
+        self.assertEqual(r[3]['Female'], 0)
+        self.assertEqual(r[3]['Total'], 1)
+
+        self.assertEqual(r[4]['Male'], 1)
+        self.assertEqual(r[4]['Female'], 0)
+        self.assertEqual(r[4]['Total'], 1)
+
+        self.assertEqual(r[5]['Male'], 1)
+        self.assertEqual(r[5]['Female'], 0)
+        self.assertEqual(r[5]['Total'], 1)
+
+        self.assertEqual(r[6]['Male'], 6)
+        self.assertEqual(r[6]['Female'], 1)
+        self.assertEqual(r[6]['Total'], 7)
