aabdbc349cfd4b32da55bd6ed3816ce5286951ae
Anders Pearson <anders@columbia.edu>
2013-08-12 14:13:14 -0400

    UserProfile tests

--- phtc/main/tests/test_models.py
+++ phtc/main/tests/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from phtc.main.models import DashboardInfo, NYLEARNS_Course_Map
-from phtc.main.models import ModuleType, SectionCss
+from phtc.main.models import ModuleType, SectionCss, user_created
+from phtc.main.models import UserProfile
 from pagetree.models import Hierarchy
 
 
@@ -34,3 +36,47 @@ class SectionCssTest(TestCase):
         root = h.get_root()
         sc = SectionCss.objects.create(section_css=root, css_field="")
         self.assertTrue(sc.edit_form() is not None)
+
+
+class FakeRequest(object):
+    POST = dict(
+        fname="test",
+        lname="user",
+        work_city="new york",
+        work_state="ny",
+        work_zip="10027",
+        sex="M",
+        age="21",
+        origin="space",
+        ethnicity="orange",
+        umc="umc",
+        employment_location="employment_location",
+        position="position",
+        dept_health="dept_health",
+        geo_dept_health="geo_dept_health",
+        experience="experience",
+        rural="rural",
+        degree="degree",
+        nylearns_course_init="nylearns_course_init",
+        nylearns_user_id="nylearns_user_id",
+    )
+
+
+class UserCreatedTest(TestCase):
+    def test_user_created(self):
+        sender = None
+        user = User.objects.create(username="testuser")
+        req = FakeRequest()
+        user_created(sender, user, req)
+        up = UserProfile.objects.get(user=user)
+        self.assertEqual(str(up), "testuser's profile")
+
+    def test_user_created_optional_fields(self):
+        sender = None
+        user = User.objects.create(username="testuser")
+        req = FakeRequest()
+        req.POST["other_position_category"] = "other position"
+        req.POST["other_location"] = "other location"
+        user_created(sender, user, req)
+        up = UserProfile.objects.get(user=user)
+        self.assertEqual(str(up), "testuser's profile")
