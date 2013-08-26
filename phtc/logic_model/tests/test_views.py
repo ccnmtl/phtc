030d8d699e62671c8c546b5e220e9d37d221b442
Anders Pearson <anders@columbia.edu>
2013-08-26 11:36:03 -0400

    logic_model.views test coverage

--- phtc/logic_model/tests/test_views.py
+++ phtc/logic_model/tests/test_views.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
+from phtc.logic_model.models import GamePhase, ActivePhase, Column
 
 
 class BasicTest(TestCase):
-
     def setUp(self):
         self.c = Client()
         self.u = User.objects.create(username="testuser")
@@ -12,16 +12,27 @@ class BasicTest(TestCase):
         self.u.save()
         self.c.login(username="testuser", password="test")
 
-    def tearDown(self):
-        self.u.delete()
+    def test_settings_get(self):
+        response = self.c.get("/_logic_model/settings/")
+        self.assertEquals(response.status_code, 403)
 
-    def test_root(self):
-        response = self.c.get("/")
+    def test_settings_post_no_aps(self):
+        response = self.c.post(
+            "/_logic_model/settings/",
+            {},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
+        )
         self.assertEquals(response.status_code, 200)
 
-    def test_reset(self):
+    def test_settings_post_populated(self):
+        gp = GamePhase.objects.create()
+        c = Column.objects.create()
+        gp2 = GamePhase.objects.create()
+        ActivePhase.objects.create(game_phase=gp, column=c)
+        ActivePhase.objects.create(game_phase=gp2, column=c)
         response = self.c.post(
-            "/",
-            dict(action="reset"),
+            "/_logic_model/settings/",
+            {},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
         )
-        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.status_code, 200)
