837c4ea90c926899935c663358b80c6352e4d6b6
coati-00 <coati.00@gmail.com>
2015-12-17 20:17:09 -0500

    changing feedback question

--- media/js/feedback.js
+++ media/js/feedback.js
@@ -3,52 +3,30 @@ jQuery(document).ready(function() {
         jQuery(this).click(function() {
             
             var casequestion = jQuery(this).parent().parent().parent();
-            console.log(casequestion);
             
             /* make array of answers */
-            var arr = jQuery(this).parent().parent().find('li input:radio'); // took off a .parent()
-            //console.log('arr');
-            //console.log(arr);
-            //console.log('arr.length()');
-            //console.log(arr.length);
+            var arr = jQuery(this).parent().parent().find('li input:radio');
             
             /* find the selected answer */
-            /* This works */
-            var radios = arr.index(':checked');
-            console.log('radios');
-            console.log(radios);
-            // jQuery('input:radio:checked:checked');
-            //var radios = arr.index('li input:radio');
-            //console.log('radios');
-            //console.log(radios);
+            var index = arr.index(':checked');
             
-            //var sel_radio = radios.find(':checked');
+            /* make array of responses to answers */
+            var feedback = jQuery(casequestion).find('.case-feedback div.item-feedback.well');
+            console.log("feedback");
+            console.log(feedback);
+            console.log("feedback[index]");
+            console.log(feedback[index]);
             
-            //var sel_radio = arr.index('input:radio'); // jQuery(arr).find('input:radio').attr('checked', true);
-            //console.log('sel_radio');
-            //console.log(sel_radio);
-            
-            var index = radios.index(sel_radio);
-            console.log(index);
-            
-            //var index = arr.index(this);
-            //console.log(index);
-            
-            /* make array of answers */
-            var feedback = jQuery(casequestion).children('.case-feedback');
-            //console.log("feedback");
-            //console.log(feedback);
-            
-            /* make array of answers */
-            var feedback_item = jQuery(casequestion)
-                .children('.case-feedback').children()[index];
-            //console.log("feedback_item");
-            //console.log(feedback_item);
+            /* display the feedback that corresponds to the selected answer */
+            var feedback_item = feedback[index];//jQuery(casequestion)
+                //.children('.case-feedback .item-feedback').children()[index];
+            console.log("feedback_item");
+            console.log(feedback_item);
 
-            jQuery(feedback).children().each(function() {
+            jQuery(feedback).each(function() {
                 jQuery(this).css('display','none');
             });
-            jQuery(this).find('input:radio').attr('checked', true);
+            //jQuery(this).find('input:radio').attr('checked', true);
 
             jQuery(feedback_item).css({
                 display: 'block'
