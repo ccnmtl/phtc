4750f9623d93005f60f55f8e0abece6a918eceb4
Anders Pearson <anders@columbia.edu>
2016-08-11 12:00:01 +0100

    django 1.10 style urls.py

--- phtc/logic_model/urls.py
+++ phtc/logic_model/urls.py
@@ -1,14 +1,12 @@
-from django.conf.urls import patterns, url
+import django.views.static
 import os.path
+from django.conf.urls import url
+from .views import settings
 
 media_root = os.path.join(os.path.dirname(__file__), "media")
 
-urlpatterns = patterns(
-    '',
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': media_root}),
-
-    url(r'^settings/$',
-        'phtc.logic_model.views.settings',
-        name='settings'),
-)
+urlpatterns = [
+    url(r'^media/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': media_root}),
+    url(r'^settings/$', settings, name='settings'),
+]
--- phtc/urls.py
+++ phtc/urls.py
@@ -1,38 +1,43 @@
+import django.contrib.auth.views
+import django.views.static
+import djangowind.views
+
 from django.conf import settings
 from django.contrib import admin
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
+
+from phtc.main.views import (
+    region2phtc, reports, dashboard, edit_page, page,
+)
 admin.autodiscover()
 
 redirect_after_logout = getattr(settings, 'LOGOUT_REDIRECT_URL', None)
-auth_urls = (r'^accounts/', include('django.contrib.auth.urls'))
-logout_page = (r'^accounts/logout/$', 'django.contrib.auth.views.logout',
-               {'next_page': redirect_after_logout})
+auth_urls = url(r'^accounts/', include('django.contrib.auth.urls'))
+logout_page = url(r'^accounts/logout/$', django.contrib.auth.views.logout,
+                  {'next_page': redirect_after_logout})
 
 if hasattr(settings, 'CAS_BASE'):
-    auth_urls = (r'^accounts/', include('djangowind.urls'))
-    logout_page = (r'^accounts/logout/$', 'djangowind.views.logout',
-                   {'next_page': redirect_after_logout})
+    auth_urls = url(r'^accounts/', include('djangowind.urls'))
+    logout_page = url(r'^accounts/logout/$', djangowind.views.logout,
+                      {'next_page': redirect_after_logout})
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     logout_page,
     auth_urls,
-    url(r'^$', view='phtc.main.views.region2phtc', name='region2phtc'),
-    url(r'^reports/$', view='phtc.main.views.reports', name='reports'),
-    (r'^admin/', include(admin.site.urls)),
-    url(r'^dashboard/', view='phtc.main.views.dashboard', name='dashboard'),
-    (r'^_stats/', TemplateView.as_view(template_name='stats.html')),
-    (r'^uploads/(?P<path>.*)$',
-     'django.views.static.serve',
-     {'document_root': settings.MEDIA_ROOT}),
-    (r'^_pagetree/', include('pagetree.urls')),
-    (r'^_quiz/', include('quizblock.urls')),
-    (r'^_rgt/', include('phtc.treatment_activity.urls')),
-    (r'^_logic_model/', include('phtc.logic_model.urls')),
-    (r'^smoketest/', include('smoketest.urls')),
+    url(r'^$', view=region2phtc, name='region2phtc'),
+    url(r'^reports/$', view=reports, name='reports'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^dashboard/', view=dashboard, name='dashboard'),
+    url(r'^_stats/', TemplateView.as_view(template_name='stats.html')),
+    url(r'^uploads/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    url(r'^_pagetree/', include('pagetree.urls')),
+    url(r'^_quiz/', include('quizblock.urls')),
+    url(r'^_rgt/', include('phtc.treatment_activity.urls')),
+    url(r'^_logic_model/', include('phtc.logic_model.urls')),
+    url(r'^smoketest/', include('smoketest.urls')),
     # these need to be last
-    (r'^edit/(?P<path>.*)$', 'phtc.main.views.edit_page',
-     {}, 'edit-page'),
-    (r'^(?P<path>.*)$', 'phtc.main.views.page'),
-)
+    url(r'^edit/(?P<path>.*)$', edit_page, {}, 'edit-page'),
+    url(r'^(?P<path>.*)$', page),
+]
