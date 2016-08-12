be692821a7ede3ac1601e4eeb27e789db62a7869
Anders Pearson <anders@columbia.edu>
2016-08-12 14:14:59 +0100

    update quizblock urls.py
    missed this one

--- quizblock/urls.py
+++ quizblock/urls.py
@@ -1,19 +1,24 @@
-from django.conf.urls import patterns
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    'quizblock.views',
-    (r'^edit_quiz/(?P<id>\d+)/$', 'edit_quiz', {}, 'edit-quiz'),
-    (r'^edit_quiz/(?P<id>\d+)/add_question/$', 'add_question_to_quiz', {},
-     'add-question-to-quiz'),
-    (r'^edit_question/(?P<id>\d+)/$', 'edit_question', {}, 'edit-question'),
-    (r'^edit_question/(?P<id>\d+)/add_answer/$', 'add_answer_to_question', {},
-     'add-answer-to-question'),
-    (r'^delete_question/(?P<id>\d+)/$', 'delete_question', {},
-     'delete-question'),
-    (r'^reorder_answers/(?P<id>\d+)/$', 'reorder_answers', {},
-     'reorder-answer'),
-    (r'^reorder_questions/(?P<id>\d+)/$', 'reorder_questions', {},
-     'reorder-questions'),
-    (r'^delete_answer/(?P<id>\d+)/$', 'delete_answer', {}, 'delete-answer'),
-    (r'^edit_answer/(?P<id>\d+)/$', 'edit_answer', {}, 'edit-answer'),
+from .views import (
+    edit_quiz, add_question_to_quiz, edit_question, add_answer_to_question,
+    delete_question, reorder_answers, reorder_questions, delete_answer,
+    edit_answer,
 )
+
+urlpatterns = [
+    url(r'^edit_quiz/(?P<id>\d+)/$', edit_quiz, {}, 'edit-quiz'),
+    url(r'^edit_quiz/(?P<id>\d+)/add_question/$', add_question_to_quiz, {},
+        'add-question-to-quiz'),
+    url(r'^edit_question/(?P<id>\d+)/$', edit_question, {}, 'edit-question'),
+    url(r'^edit_question/(?P<id>\d+)/add_answer/$', add_answer_to_question, {},
+        'add-answer-to-question'),
+    url(r'^delete_question/(?P<id>\d+)/$', delete_question, {},
+        'delete-question'),
+    url(r'^reorder_answers/(?P<id>\d+)/$', reorder_answers, {},
+        'reorder-answer'),
+    url(r'^reorder_questions/(?P<id>\d+)/$', reorder_questions, {},
+        'reorder-questions'),
+    url(r'^delete_answer/(?P<id>\d+)/$', delete_answer, {}, 'delete-answer'),
+    url(r'^edit_answer/(?P<id>\d+)/$', edit_answer, {}, 'edit-answer'),
+]
